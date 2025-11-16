ffb7c29266964a73b136d1d7da670ae9cdf30f0d
GH-Tyron <110431714+GH-Tyron@users.noreply.github.com>
2025-11-16 20:54:49 +0000

    Create main.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/main.ts
@@ -0,0 +1,40 @@
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+import Cpp = Subjects.Cpp;
+import Java = Subjects.Java;
+import React = Subjects.React;
+import Teacher = Subjects.Teacher;
+
+// Create and export the constants
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+// Create and export teacher
+export const cTeacher: Teacher = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  experienceTeachingC: 10
+};
+
+// Cpp
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
